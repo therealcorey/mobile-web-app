fee4e6c7dfa1bdfcaea242d4c8de3e4512829c8f
therealcorey <33095358+therealcorey@users.noreply.github.com>
2019-06-03 17:49:03 -0400

    sql

--- elections_database2.sql
+++ elections_database2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 03, 2019 at 08:27 PM
+-- Generation Time: Jun 03, 2019 at 09:48 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -37,35 +37,14 @@ CREATE TABLE IF NOT EXISTS `candidate` (
   `district` int(10) NOT NULL,
   PRIMARY KEY (`candidate_id`),
   KEY `party_name` (`party_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `candidate`
 --
 
 INSERT INTO `candidate` (`candidate_id`, `party_name`, `votes`, `year`, `district`) VALUES
-(1, 'A Partnership for National Unity/Alliance for Change', 2788, 2015, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `database_administrator`
---
-
-DROP TABLE IF EXISTS `database_administrator`;
-CREATE TABLE IF NOT EXISTS `database_administrator` (
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `database_administrator`
---
-
-INSERT INTO `database_administrator` (`first_name`, `last_name`, `username`, `password`) VALUES
-('Corey', 'Young', 'admin', 'admin1');
+(1, 'People\'s Progressive Party/Civic\r\n', 27, 2015, 1);
 
 -- --------------------------------------------------------
 
@@ -79,17 +58,17 @@ CREATE TABLE IF NOT EXISTS `election` (
   `district` int(10) NOT NULL,
   `pollingstation` varchar(100) NOT NULL,
   `votes` bigint(20) NOT NULL,
-  PRIMARY KEY (`candidate_id`),
+  UNIQUE KEY `candidate_id` (`candidate_id`),
   KEY `pollingstation` (`pollingstation`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `election`
 --
 
 INSERT INTO `election` (`candidate_id`, `district`, `pollingstation`, `votes`) VALUES
-(2, 1, 'Baramita Primary', 27),
 (1, 1, 'Baramita Primary', 13),
+(2, 1, 'Baramita Primary', 27),
 (3, 1, 'Baramita Primary', 2),
 (4, 1, 'Baramita Primary', 0);
 
@@ -108,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `miscellaneous` (
   `unmarked_ballots` mediumint(9) NOT NULL,
   `total_ballot_boxes` mediumint(9) NOT NULL,
   PRIMARY KEY (`pollingstation`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `miscellaneous`
@@ -125,7 +104,7 @@ INSERT INTO `miscellaneous` (`district`, `pollingstation`, `total_valid_votes`, 
 
 DROP TABLE IF EXISTS `party`;
 CREATE TABLE IF NOT EXISTS `party` (
-  `party_id` int(11) NOT NULL,
+  `party_id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `party_name` varchar(100) NOT NULL,
   `ideology` varchar(200) DEFAULT NULL,
   `existence` date DEFAULT NULL,
@@ -134,18 +113,19 @@ CREATE TABLE IF NOT EXISTS `party` (
   `chairperson_first_name` varchar(50) NOT NULL,
   `chairperson_last_name` varchar(50) NOT NULL,
   `chairperson_DOB` date DEFAULT NULL,
-  PRIMARY KEY (`party_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`party_id`),
+  UNIQUE KEY `party_name` (`party_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `party`
 --
 
 INSERT INTO `party` (`party_id`, `party_name`, `ideology`, `existence`, `election_symbol`, `abbreviation`, `chairperson_first_name`, `chairperson_last_name`, `chairperson_DOB`) VALUES
-(0, 'A Partnership for National Unity/Alliance for Change', NULL, '2015-03-07', NULL, 'APNU/AFC', 'David', 'Granger', '1942-07-15'),
-(0, 'People\'s Progressive Party/Civic\r\n', NULL, '1950-01-01', NULL, 'PPP/C', 'Bharrat', 'Jagdeo', '1964-01-23'),
-(0, 'The United Force', NULL, '1960-10-05', NULL, 'TUF', 'Manzoor', 'Nadir', NULL),
-(0, 'United Republican Party\r\n', NULL, NULL, NULL, 'URP', 'Vishnu', 'Bandhu', '1951-12-04');
+(1, 'A Partnership for National Unity/Alliance for Change', NULL, '2015-03-07', NULL, 'APNU/AFC', 'David', 'Granger', '1942-07-15'),
+(2, 'People\'s Progressive Party/Civic\r\n', NULL, '1950-01-01', NULL, 'PPP/C', 'Bharrat', 'Jagdeo', '1964-01-23'),
+(3, 'The United Force', NULL, '1960-10-05', NULL, 'TUF', 'Manzoor', 'Nadir', NULL),
+(4, 'United Republican Party\r\n', NULL, NULL, NULL, 'URP', 'Vishnu', 'Bandhu', '1951-12-04');
 
 -- --------------------------------------------------------
 
@@ -171,6 +151,23 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`user_id`, `user_email`, `user_name`, `user_password`) VALUES
 (1, 'john@doe.com', 'John Doe', '$2y$10$vZJy7y4uqQQTRN3zdi2RE.5ZJJzGEEPnzEjFXm4nEOx023XQ2Qe..'),
 (2, 'corey_young2k4@yahoo.com', 'corey', '$2y$10$aAR17bxnwRCUHAOqMQcqRuUKVIOggLUBU.ItfhbHS0FBYc2lMgSf.');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `candidate`
+--
+ALTER TABLE `candidate`
+  ADD CONSTRAINT `candidate_ibfk_1` FOREIGN KEY (`party_name`) REFERENCES `party` (`party_name`),
+  ADD CONSTRAINT `candidate_ibfk_2` FOREIGN KEY (`candidate_id`) REFERENCES `election` (`candidate_id`);
+
+--
+-- Constraints for table `miscellaneous`
+--
+ALTER TABLE `miscellaneous`
+  ADD CONSTRAINT `miscellaneous_ibfk_1` FOREIGN KEY (`pollingstation`) REFERENCES `election` (`pollingstation`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
